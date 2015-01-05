73f5b0c3def8a6af2f85dbb82e17fa9a2c0c87ae
Rafael Garcia <rafael.garcia@clever.com>
2015-01-05 10:08:57 -0800

    test killing a long-running op against a real server
    doesn't work right now since it appears that admin commands aren't kill-able

--- main_test.go
+++ main_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
 
+	"github.com/facebookgo/mgotest"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -81,5 +84,48 @@ func TestDirectConnect(t *testing.T) {
 		if err := validateMongoURL(passingtest); err != nil {
 			t.Fatalf("valid URL should validate: %s", passingtest)
 		}
+	}
+}
+
+func TestIntegration(t *testing.T) {
+	replset := mgotest.NewReplicaSet(2, t)
+	defer replset.Stop()
+
+	// run whackanop against each server
+	for _, addr := range replset.Addrs() {
+		session, err := mgo.Dial(fmt.Sprintf("%s?connect=direct", addr))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, false)
+		wao := WhackAnOp{
+			OpFinder: MongoOpFinder{session},
+			OpKiller: MongoOpKiller{session},
+			Query:    bson.M{"op": "query", "secs_running": bson.M{"$gt": 1}},
+			Tick:     time.NewTicker(time.Second).C,
+			Debug:    false,
+			Verbose:  true,
+		}
+		go func() {
+			if err := wao.Run(); err != nil {
+				t.Fatal(err)
+			}
+		}()
+	}
+
+	// run an op against each server, ensure ops get killed
+	runBadOp := func(addr string) {
+		session, err := mgo.Dial(fmt.Sprintf("%s?connect=direct", addr))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer session.Close()
+		if err := session.DB("admin").Run(bson.M{"sleep": 1, "secs": 20}, nil); err != nil {
+			t.Fatal(err)
+		}
+	}
+	for _, addr := range replset.Addrs() {
+		runBadOp(addr)
 	}
 }
